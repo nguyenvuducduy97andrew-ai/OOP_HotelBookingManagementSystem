0f8626d9ab4ea8151725d676ea296cdb7a54bf0e
Nguyễn Vũ Đức Duy <nguyenvuducduy97andrew@gmail.com>
2026-08-07 14:55:07 +0700

    Structural organising finalization

---
+++ src/pch/core_pch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QDate>
+#include <QDateTime>
+#include <QList>
+#include <QRegularExpression>
+#include <QString>
+#include <QStringList>
+#include <QTime>
+
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
---
+++ src/pch/widgets_pch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "core_pch.h"
+
+#include <QApplication>
+#include <QComboBox>
+#include <QDialog>
+#include <QFrame>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QTableWidget>
+#include <QVBoxLayout>
+#include <QWidget>
